c101ce3ace6fbcd7ec76917371249abd84b2ff81
Merwin Hill <merwin@q42.nl>
2023-04-20 16:20:25 +0200

    fix: Set body as storage
    https://docs.atlassian.com/atlassian-confluence/5.8.8/com/atlassian/confluence/api/model/content/ContentRepresentation.html

--- swagger/swagger.go
+++ swagger/swagger.go
@@ -160,6 +160,7 @@ func ParseSwagger(rootID int, content []byte, isIndex bool,
 
 	if GrabAuthors {
 		f.Body = append(f.Body, []byte(capGit(path))...)
+		f.BodyRepresentation = "storage"
 	}
 
 	return f, nil
